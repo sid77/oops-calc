2c51af438cddd8d9a25c4dfef244365b37e93cb5
sid77 <sid77@users.noreply.github.com>
2023-01-11 18:19:01 +0000

    Count distinct hands

--- oops-calc.py
+++ oops-calc.py
@@ -4,47 +4,109 @@ from math import comb
 
 
 _HAND_SIZE = 7
+_DECK_SIZE = 61
+# see README.md for these magic numbers
+_SPY_EFFECTS = 8
+_LOTUS_PETALS = 4
+_DARK_RITUALS = 4
+_OTHER_MANA = 23
+_OTHER_CARDS = 22
+_AGADEEMS = 4
+_TURNTIMBERS = 4
 
 
-def _sum_hands(spy_effects, black_mana, dark_ritual, other_mana, deck_size):
-    other_cards = deck_size - spy_effects - black_mana - dark_ritual - other_mana
-    winning_hands = 0
-    for a in range(1, 5):
-        for b in range(1, 5):
-            for c in range(1, 5):
-                for d in range(1, 5):
-                    e = _HAND_SIZE - a - b - c - d
-                    if e < 0:
-                        # this hand is invalid
-                        pass
-                    else:
-                        hands = 1
-                        hands *= comb(spy_effects, a)
-                        hands *= comb(black_mana, b)
-                        hands *= comb(dark_ritual, c)
-                        hands *= comb(other_mana, d)
-                        hands *= comb(other_cards, e)
-                        winning_hands += hands
-    return winning_hands
-
-
-def _compute(deck_size):
+def _compute(deck_size=_DECK_SIZE, other_cards=_OTHER_CARDS):
     winning_hands = 0
     total_hands = comb(deck_size, _HAND_SIZE)
     # Lotus Petal
-    winning_hands += _sum_hands(
-        spy_effects=8, black_mana=4, dark_ritual=4, other_mana=23, deck_size=deck_size
+    assert (
+        _LOTUS_PETALS + _DARK_RITUALS + _SPY_EFFECTS + _OTHER_MANA + other_cards
+        == deck_size
     )
+    rest_of_deck = deck_size - _LOTUS_PETALS - _DARK_RITUALS - _SPY_EFFECTS
+    # lol
+    # assert _LOTUS_PETALS + _DARK_RITUALS + _SPY_EFFECTS + rest_of_deck == deck_size
+    for lotus_petals in range(1, 5):
+        for dark_rituals in range(1, 5):
+            for spy_effects in range(1, 5):
+                if lotus_petals == 1 and dark_rituals == 1:
+                    # need extra mana
+                    for other_mana in range(1, 5):
+                        fillers = (
+                            _HAND_SIZE
+                            - lotus_petals
+                            - dark_rituals
+                            - spy_effects
+                            - other_mana
+                        )
+                        if fillers >= 0:
+                            hands = 1
+                            hands *= comb(_LOTUS_PETALS, lotus_petals)
+                            hands *= comb(_DARK_RITUALS, dark_rituals)
+                            hands *= comb(_SPY_EFFECTS, spy_effects)
+                            hands *= comb(_OTHER_MANA, other_mana)
+                            hands *= comb(other_cards, fillers)
+                            winning_hands += hands
+                else:
+                    fillers = _HAND_SIZE - lotus_petals - dark_rituals - spy_effects
+                    if fillers >= 0:
+                        hands = 1
+                        hands *= comb(_LOTUS_PETALS, lotus_petals)
+                        hands *= comb(_DARK_RITUALS, dark_rituals)
+                        hands *= comb(_SPY_EFFECTS, spy_effects)
+                        hands *= comb(rest_of_deck, fillers)
+                        winning_hands += hands
     # Agadeem, the Undercrypt
-    winning_hands += _sum_hands(
-        spy_effects=8, black_mana=4, dark_ritual=4, other_mana=19, deck_size=deck_size
+    adjusted_other_mana = _OTHER_MANA - _AGADEEMS - _TURNTIMBERS
+    adjusted_other_cards = other_cards + _TURNTIMBERS
+    assert (
+        _LOTUS_PETALS
+        + _AGADEEMS
+        + _DARK_RITUALS
+        + _SPY_EFFECTS
+        + adjusted_other_mana
+        + adjusted_other_cards
+        == deck_size
     )
-    # Remove the hands with both Lotus Petal and Agadeem, the Undercrypt since
-    # they have been added twice. One card is black_mana and the other is
-    # other_mana.
-    winning_hands -= _sum_hands(
-        spy_effects=8, black_mana=4, dark_ritual=4, other_mana=4, deck_size=deck_size
+    rest_of_deck -= _AGADEEMS
+    assert (
+        _LOTUS_PETALS + _AGADEEMS + _DARK_RITUALS + _SPY_EFFECTS + rest_of_deck
+        == deck_size
     )
+    for agadeems in range(1, 5):
+        for dark_rituals in range(1, 5):
+            for spy_effects in range(1, 5):
+                if dark_rituals == 1:
+                    # need extra mana
+                    for other_mana in range(1, 5):
+                        fillers = (
+                            _HAND_SIZE
+                            - agadeems
+                            - dark_rituals
+                            - spy_effects
+                            - other_mana
+                        )
+                        if fillers >= 0:
+                            hands = 1
+                            hands *= comb(_LOTUS_PETALS, 0)
+                            hands *= comb(_AGADEEMS, agadeems)
+                            hands *= comb(_DARK_RITUALS, dark_rituals)
+                            hands *= comb(_SPY_EFFECTS, spy_effects)
+                            hands *= comb(adjusted_other_mana, other_mana)
+                            hands *= comb(adjusted_other_cards, fillers)
+                            winning_hands += hands
+                else:
+                    fillers = (
+                        _HAND_SIZE - agadeems - dark_rituals - spy_effects - other_mana
+                    )
+                    if fillers >= 0:
+                        hands = 1
+                        hands *= comb(_LOTUS_PETALS, 0)
+                        hands *= comb(_AGADEEMS, agadeems)
+                        hands *= comb(_DARK_RITUALS, dark_rituals)
+                        hands *= comb(_SPY_EFFECTS, spy_effects)
+                        hands *= comb(rest_of_deck, fillers)
+                        winning_hands += hands
     probability = winning_hands / total_hands
     print(
         f"The probability of a 4 cards hand in a {deck_size} cards deck is {probability}"
@@ -52,6 +114,5 @@ def _compute(deck_size):
 
 
 if __name__ == "__main__":
-    """See README.md"""
-    _compute(61)
-    _compute(60)
+    _compute(61, 22)
+    _compute(60, 21)
